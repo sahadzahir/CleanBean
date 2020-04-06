59c0a8363bfc1bd22850f38ff15cbab5b8c94aff
cannotzho <62131975+cannotzho@users.noreply.github.com>
2020-04-06 23:41:47 +0800

    Update Cleanbean.py
    Added menu, three screens for three different floors, bin icons and trash level indicator. Please don't start updating for more than one floor or the whole app starts lagging like crazy.

--- Cleanbean.py
+++ Cleanbean.py
@@ -4,6 +4,8 @@ kivy.require('1.9.0')
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from pythymiodw import *
 from libdw import pyrebase
@@ -11,7 +13,8 @@ from kivy.properties import ObjectProperty
 import time
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivy.core.window import Window
+Window.clearcolor = (1, 1, 1, 1)
 
 projectid = "cleanbean-9e2f5"
 dburl = "https://" + projectid + ".firebaseio.com"
@@ -40,62 +43,238 @@ incoming = fbinfo.val()
 
 # You can create your kv code in the Python file
 Builder.load_string("""
-<ScreenOne>:
-    bin1: bin1
-    bin2: bin2
-    GridLayout:
-        
-        
-        id: bins
-        rows: 2
-        padding: 40
-        spacing: 80
-        
-            
-                 
+<Label>:
+    markup: True
+    color: 0, 0, 0, 1
                     
-        BoxLayout:
-            Button:
-                id: bin1
-                background_normal: 'red.png'
-                text: " %f" % root.bingetter(1)
-                size_hint: 0.5, 0.0
-        BoxLayout:        
-            Button:
-                id: bin2
-                background_normal: 'red.png'
-                text: " %f" % root.bingetter(2)
-                size_hint: 0.5, 0.0         
-                
-        BoxLayout:
-            Button:
-                text: "refresh"
-                on_press: root.update()
-            Button:
-                text: "start updating"
-                on_press: root.startclock()
-        BoxLayout:
-            Button:
-                text: "Go to Screen 2"
-                on_press:
-                    # You can define the duration of the change
-                    # and the direction of the slide
-                    root.manager.transition.direction = 'left'
-                    root.manager.transition.duration = 1
-                    root.manager.current = 'screen_two'
-     
-<ScreenTwo>:
+<BinButt@Button>:
+    font_size: 32
+    color: 0, 0, 0, 1
+    background_normal: 'dustbin.png'
+    size: 50, 50
+    size_hint: .3, .5                
+
+<BinBar@Button>:
+    
+    color: 0, 0, 0, 1
+    size: 50, 50
+    size_hint: .115, .31
+
+
+
+<MainMenu>:
     BoxLayout:
+        orientation: "vertical"
+        spacing: 50
+        padding: 50
         Button:
-            text: "Go to Screen 1"
+            text: "Floor 3"
             on_press:
                 root.manager.transition.direction = 'right'
                 root.manager.current = 'screen_one'
+                
+        Button:
+            text: "Floor 4"
+            on_press:
+                root.manager.transition.direction = 'right'
+                root.manager.current = 'screen_two'
+                
+        Button:
+            text: "Floor 5"
+            on_press:
+                root.manager.transition.direction = 'right'
+                root.manager.current = 'screen_three'
+<FloorScreenOne>:
+    bin1: bin1
+    bin2: bin2
+    BoxLayout:
+                        
+        id: bins
+        orientation: "vertical"
+        spacing: 10
+        padding: 10
+        
+        FloatLayout:
+            BinBar
+                background_normal: 'grad5.png'
+                pos_hint: {'right': 0.358, 'top': 0.68}
+            BinBar:
+                id: bin1
+                background_normal: ''
+                
+                pos_hint: {'right': 0.358, 'top': 0.68}
+            BinButt:
+                pos_hint: {'right': 0.45, 'top': 0.8}
+                
+            BinBar
+                background_normal: 'grad5.png'
+                pos_hint: {'right': 0.758, 'top': 0.68}
+            BinBar:
+                id: bin2
+                background_normal: ''
+                
+                pos_hint: {'right': 0.758, 'top': 0.68}
+            BinButt:
+                pos_hint: {'right': 0.85, 'top': 0.8}
+            Label:
+                font_size: 32
+                text: "Building 1, Floor 3"
+                pos_hint: {'right': 1, 'top': 1.45}
+            Label:
+                font_size: 16
+                text: "1.313"
+                pos_hint: {'right': 0.8, 'top': 0.8}
+                
+            Label:
+                font_size: 16
+                text: "1.314"
+                pos_hint: {'right': 1.2, 'top': 0.8}
+
+                 
+
+        Button:
+            text: "start updating"
+            size_hint: 1, 0.2
+            on_press: root.startclock()
+
+        Button:
+            text: "Back"
+            size_hint: 1, 0.2                        
+            on_press:
+                # You can define the duration of the change
+                # and the direction of the slide
+                root.manager.transition.direction = 'left'
+                root.manager.transition.duration = 1
+                root.manager.current = 'menu'
+
+<FloorScreenTwo>:
+    bin1: bin1
+    bin2: bin2
+    BoxLayout:
+                        
+        id: bins
+        orientation: "vertical"
+        spacing: 10
+        padding: 10
+        
+        FloatLayout:
+            BinBar
+                background_normal: 'grad5.png'
+                pos_hint: {'right': 0.358, 'top': 0.68}
+            BinBar:
+                id: bin1
+                background_normal: ''
+                
+                pos_hint: {'right': 0.358, 'top': 0.68}
+            BinButt:
+                pos_hint: {'right': 0.45, 'top': 0.8}
+                
+            BinBar
+                background_normal: 'grad5.png'
+                pos_hint: {'right': 0.758, 'top': 0.68}
+            BinBar:
+                id: bin2
+                background_normal: ''
+                
+                pos_hint: {'right': 0.758, 'top': 0.68}
+            BinButt:
+                pos_hint: {'right': 0.85, 'top': 0.8}
+            Label:
+                font_size: 32
+                text: "Building 1, Floor 4"
+                pos_hint: {'right': 1, 'top': 1.45}
+            Label:
+                font_size: 16
+                text: "1.413"
+                pos_hint: {'right': 0.8, 'top': 0.8}
+                
+            Label:
+                font_size: 16
+                text: "1.414"
+                pos_hint: {'right': 1.2, 'top': 0.8}
+                 
+
+        Button:
+            text: "start updating"
+            size_hint: 1, 0.2
+            on_press: root.startclock()
+
+        Button:
+            text: "Back"
+            size_hint: 1, 0.2                        
+            on_press:
+                # You can define the duration of the change
+                # and the direction of the slide
+                root.manager.transition.direction = 'left'
+                root.manager.transition.duration = 1
+                root.manager.current = 'menu'
+                
+<FloorScreenThree>:
+    bin1: bin1
+    bin2: bin2
+    BoxLayout:
+                        
+        id: bins
+        orientation: "vertical"
+        spacing: 10
+        padding: 10
+        
+        FloatLayout:
+                    
+            BinBar
+                background_normal: 'grad5.png'
+                pos_hint: {'right': 0.358, 'top': 0.68}
+            BinBar:
+                id: bin1
+                background_normal: ''
+                
+                pos_hint: {'right': 0.358, 'top': 0.68}
+            BinButt:
+                pos_hint: {'right': 0.45, 'top': 0.8}
+                
+            BinBar
+                background_normal: 'grad4.png'
+                pos_hint: {'right': 0.758, 'top': 0.68}
+            BinBar:
+                id: bin2
+                background_normal: ''
+                
+                pos_hint: {'right': 0.758, 'top': 0.68}
+            BinButt:
+                pos_hint: {'right': 0.85, 'top': 0.8}
+            Label:
+                font_size: 32
+                text: "Building 1, Floor 5"
+                pos_hint: {'right': 1, 'top': 1.45}
+            Label:
+                font_size: 16
+                text: "1.513"
+                pos_hint: {'right': 0.8, 'top': 0.8}
+                
+            Label:
+                font_size: 16
+                text: "1.514"
+                pos_hint: {'right': 1.2, 'top': 0.8}
+
+        Button:
+            text: "start updating"
+            size_hint: 1, 0.2
+            on_press: root.startclock()
+
+        Button:
+            text: "Back"
+            size_hint: 1, 0.2                        
+            on_press:
+                # You can define the duration of the change
+                # and the direction of the slide
+                root.manager.transition.direction = 'left'
+                root.manager.transition.duration = 1
+                root.manager.current = 'menu'
 """)
  
 # Create a class for all screens in which you can include
 # helpful methods specific to that screen
-class ScreenOne(Screen):
+class FloorScreenOne(Screen):
     bin1 = ObjectProperty(None)
     bin2 = ObjectProperty(None)
     def message(self):
@@ -105,15 +284,44 @@ class ScreenOne(Screen):
     def update(self, dt):
         binlist = [self.bin1, self.bin2]
         for binno, bins in enumerate(binlist):
-            bins.size_hint[1] = self.bingetter(binno+1)
-            bins.text = " %s" % self.bingetter(binno+1)
+            bins.size_hint[1] = self.bingetter(binno+1)*0.3
+            
     def startclock(self):
-        Clock.schedule_interval(self.update, 1)
+        Clock.schedule_interval(self.update, 0.1)
     pass
  
- 
- 
-class ScreenTwo(Screen):
+class FloorScreenTwo(Screen):
+    bin1 = ObjectProperty(None)
+    bin2 = ObjectProperty(None)
+    def message(self):
+        print("Size changed")
+    def bingetter(self, nom):
+        return(firebase.database().child("Bin_%s"% str(nom)).get(user['idToken']).val())
+    def update(self, dt):
+        binlist = [self.bin1, self.bin2]
+        for binno, bins in enumerate(binlist):
+            bins.size_hint[1] = self.bingetter(binno+1)*0.3
+            
+    def startclock(self):
+        Clock.schedule_interval(self.update, 3)
+    pass 
+
+class FloorScreenThree(Screen):
+    bin1 = ObjectProperty(None)
+    bin2 = ObjectProperty(None)
+    def message(self):
+        print("Size changed")
+    def bingetter(self, nom):
+        return(firebase.database().child("Bin_%s"% str(nom)).get(user['idToken']).val())
+    def update(self, dt):
+        binlist = [self.bin1, self.bin2]
+        for binno, bins in enumerate(binlist):
+            bins.size_hint[1] = self.bingetter(binno+1)*0.3
+            
+    def startclock(self):
+        Clock.schedule_interval(self.update, 3)
+    pass 
+class MainMenu(Screen):
     pass
  
  
@@ -122,8 +330,10 @@ screen_manager = ScreenManager()
  
 # Add the screens to the manager and then supply a name
 # that is used to switch screens
-screen_manager.add_widget(ScreenOne(name="screen_one"))
-screen_manager.add_widget(ScreenTwo(name="screen_two"))
+screen_manager.add_widget(MainMenu(name="menu"))
+screen_manager.add_widget(FloorScreenOne(name="screen_one"))
+screen_manager.add_widget(FloorScreenTwo(name="screen_two"))
+screen_manager.add_widget(FloorScreenThree(name="screen_three"))
  
 class CleanBeanApp(App):
  
